d9ae661a7f35fe6cfb4d5259abf05b81221279f5
Massimiliano Testa <massimilianotesta16@gmail.com>
2026-03-19 10:03:18 +0100

    modificato il config

--- Server/config.py
+++ Server/config.py
@@ -1,10 +1,12 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///poliwood.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
